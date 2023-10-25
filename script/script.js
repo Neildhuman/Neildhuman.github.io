291bfa0546ba65448f919a694d80de8cd03aca77
Neildhuman <neilleonarda@gmail.com>
2023-10-25 20:24:14 +0800

    initial commit

---
+++ script/script.js
@@ -0,0 +1,34 @@
+// const age = 0;
+
+// if (age <= 12 && age >= 1) {
+//     alert("Child")
+// } else if (age >= 13 && age <= 21) {
+//     alert("Teenager")
+// } else if (age >= 22 && age <= 59) {
+//     alert("Adult")
+// } else if (age >= 60) {
+//     alert("Senior")
+// } else {
+//     alert("Error")
+// }
+
+// for (let i = 0; i=100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//       }
+//     }
+
+// let i = 100;
+// while (i >= 1) {
+//     if (i % 2 === 0){ 
+//         console.log(i)
+//         i--
+//     }
+// }
+
+// const body = document.body
+// const div = document.querySelector('div')
+
+// const spanHi = document,querySelector(#hi)
+
+// body.append("Hello World")
